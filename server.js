fd225ea737d55f0f89085da2943e1f85d6269dcb
griffinjoshs <leveloutusa@gmail.com>
2021-03-25 18:05:56 -0400

    heroku stuff

--- server.js
+++ server.js
@@ -16,8 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+    app.use(express.static("client/build"));
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 require("./server/routes/subscriber.routes")(app);
 require("./server/routes/nodeMailerRoutes")(app);

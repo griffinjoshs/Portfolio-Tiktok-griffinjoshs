4f91f9ee294e208c56e37ffb80383ab73896c54b
griffinjoshs <leveloutusa@gmail.com>
2021-03-25 18:21:18 -0400

    fdds

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+// const cors = require("cors");
 const PORT = process.env.PORT || 8000;
 const mongoose = require("mongoose");
 // require("dotenv").config({ path: "./config.env" });
@@ -11,9 +11,9 @@ const mongoose = require("mongoose");
 
 // require('./server/config/mongoose.config')
 
-app.use(cors());
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
